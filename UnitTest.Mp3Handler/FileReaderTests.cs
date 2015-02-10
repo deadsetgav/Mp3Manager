6ae022d562ab5a82e8caf1e8f878479d91e18dd9
deadsetgav <gavinjopson@hotmail.co.uk>
2015-02-10 20:51:59 +0000

    First failing test

---
+++ UnitTest.Mp3Handler/FileReaderTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mp3Handler;
+
+namespace UnitTest.Mp3Handler
+{
+    [TestClass]
+    public class FileReaderTests
+    {
+        [TestMethod]
+        public void GivenFilename_ReadMp3File()
+        {
+            // Arrange
+            var fullFilePath = @"D:\testing\Ride\Going Blank Again\01 - Leave Them All Behind.mp3";
+            var handler = new Handler();
+
+            // Act
+            var mp3 = handler.Get(fullFilePath);
+
+            // Assert
+            Assert.AreEqual(fullFilePath, mp3.FullFilePath);
+
+        }
+    }
+}
