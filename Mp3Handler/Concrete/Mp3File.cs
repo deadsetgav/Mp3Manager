ebd5d5b0c558327ed460754daa807feb7bbc8e98
deadsetgav <gavinjopson@hotmail.co.uk>
2015-02-10 21:15:59 +0000

    missed off some files on the commit

---
+++ Mp3Handler/Concrete/Mp3File.cs
@@ -0,0 +1,31 @@
+﻿using Common.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mp3Handler.Concrete
+{
+    internal class Mp3File : IMp3
+    {
+        private string _fullFilePath;
+        private string _albumArtist;
+
+        public Mp3File(string fullFilePath)
+        {
+            this._fullFilePath = fullFilePath;
+            this._albumArtist = "Ride";
+        }
+
+        public string FullFilePath
+        {
+            get { return _fullFilePath; }
+        }
+
+        public string AlbumArtist
+        {
+            get { return _albumArtist; }
+        }
+    }
+}
---
+++ Mp3Handler/Handler.cs
@@ -0,0 +1,18 @@
+﻿using Common.Abstract;
+using Mp3Handler.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mp3Handler
+{
+    public class Handler
+    {
+        public IMp3 Get(string fullFilePath)
+        {
+            return new Mp3File(fullFilePath);
+        }
+    }
+}
