e5f38ebcdfa95690c04d596f9eece02db2f18a71
deadsetgav <gavinjopson@hotmail.co.uk>
2015-02-10 20:21:06 +0000

    Fresh start
    Gonna start again. try to stick to TDD, SOLID and clean code!

---
+++ Mp3Manager/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mp3Manager
+{
+    public class Class1
+    {
+    }
+}
