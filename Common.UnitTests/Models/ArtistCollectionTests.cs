f05521ea515934745b27358f96dbf394f5037719
deadsetgav <gavinjopson@hotmail.co.uk>
2015-02-17 21:12:16 +0000

    More tests around the collection

---
+++ Common.UnitTests/Models/ArtistCollectionTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Models;
+
+namespace Common.UnitTests.Models
+{
+    [TestClass]
+    public class ArtistCollectionTests
+    {
+        [TestMethod]
+        public void WithArtistCollection_CanCreate()
+        {            
+            // Act
+            var collection = new ArtistCollection();
+
+            // Assert
+            Assert.AreEqual(0, collection.Artists.Count());
+        }
+
+        [TestMethod]
+        public void WithArtistCollection_CanAddArtist()
+        {
+            // Arrange
+            var artist = new Artist("Slayer");
+            var collection = new ArtistCollection();
+
+            // Act
+            collection.Add(artist);
+
+            // Assert
+            Assert.AreEqual(1, collection.Artists.Count());
+        }
+
+        [TestMethod]
+        public void WithArtistCollection_CanAddTwoArtists()
+        {
+            // Arrange
+            var artist1 = new Artist("Megadeth");
+            var artist2 = new Artist("Testament");
+            var collection = new ArtistCollection();
+
+            // Act
+            collection.Add(artist1);
+            collection.Add(artist2);
+
+            // Assert
+            Assert.AreEqual(2, collection.Artists.Count());
+        }
+
+        [TestMethod]
+        public void WithArtistCollection_CannotAddSameArtistTwice()
+        {
+            // Arrange
+            var artist = new Artist("Anthrax");
+            var collection = new ArtistCollection();
+
+            // Act
+            collection.Add(artist);
+            collection.Add(artist);
+
+            // Assert
+            Assert.AreEqual(1, collection.Artists.Count());
+        }
+
+    }
+}
