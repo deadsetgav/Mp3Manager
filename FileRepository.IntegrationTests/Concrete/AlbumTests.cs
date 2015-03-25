9c297b75837b48468a0edc66a9ef721d5128c18f
deadsetgav <gavinjopson@hotmail.co.uk>
2015-03-25 20:49:27 +0000

    dont know why these files weren't tracked

---
+++ FileRepository.IntegrationTests/Concrete/AlbumTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileRepository.IntegrationTests.TestObjects;
+using FileRepository.Handlers;
+using FileRepository.Concrete;
+using Common.Abstract;
+using FileRepository.UnitTests.TestObjects;
+
+namespace FileRepository.IntegrationTests.Concrete
+{
+    [TestClass]
+    public class AlbumTests : IntegrationTestBase
+    {
+        [TestMethod, TestCategory(Integration)]
+        public void Album_CanCreate()
+        {
+            // Arrange
+            var directory = new MusicDirectoryReader(@"D:\testing\Ride\Going Blank Again");
+            var mp3Handler = new Mp3Handler(new TestWriter());
+            
+            // Act
+            var album = new AlbumHandler(directory, mp3Handler);
+            
+            // Assert
+            Assert.IsNotNull(album);  
+        }
+
+        [TestMethod, TestCategory(Integration)]
+        public void Album_CanSeeTracks()
+        {
+            // Arrange
+            var album = GoingBlankAgain().Populate();
+
+            // Act
+            var tracks = album.Tracks;
+            var track = tracks.First();
+
+            // Assert
+            Assert.AreEqual(10, tracks.Count());
+            Assert.IsInstanceOfType(track, typeof(IMp3Metadata));
+            Assert.AreEqual("Going Blank Again",track.Album);
+
+        }
+
+   
+    
+
+        #region Private Helpers
+        
+        private AlbumHandler GoingBlankAgain()
+        {
+            var directory = new MusicDirectoryReader(@"D:\testing\Ride\Going Blank Again");
+            var mp3Handler = new Mp3Handler(new TestWriter());
+
+            // Act
+            return new AlbumHandler(directory, mp3Handler);
+        }
+
+        #endregion
+
+    }
+}
---
+++ FileRepository/Handlers/AlbumHandler.cs
@@ -0,0 +1,44 @@
+﻿using Common.Abstract;
+using Common.Models;
+using FileRepository.Abstract;
+using FileRepository.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRepository.Handlers
+{
+    class AlbumHandler 
+    {
+
+        // TODO : This will need to know how to save albums / tracks and pass itself up to the Album class in common
+        private MusicDirectoryReader _directoryReader;
+        private IMp3Reader _mp3Reader;
+       
+        public AlbumHandler(MusicDirectoryReader directoryReader, IMp3Reader mp3Reader)
+        {
+            _directoryReader = directoryReader;
+            _mp3Reader = mp3Reader;
+        }
+
+        public IAlbum Populate()
+        {
+            return new Album(PopulateTracks());
+        }
+        
+        private List<IMp3> PopulateTracks()
+        {
+            var tracks = new List<IMp3>();
+            foreach (var file in _directoryReader.GetMusicFiles())
+            {
+                tracks.Add(_mp3Reader.Get(file.FullName));
+            }
+            return tracks;
+        }
+
+        
+
+    }
+}
